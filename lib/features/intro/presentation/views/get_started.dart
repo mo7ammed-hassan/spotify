c85f1200ae8749508225290fe760757be5b73c87
mohamedhassan <mohamed7github@gmail.com>
2024-09-23 22:37:01 +0300

    add navigator to ChooseModeView

--- lib/features/intro/presentation/views/get_started.dart
+++ lib/features/intro/presentation/views/get_started.dart
@@ -4,6 +4,7 @@ import 'package:spotify/common/widgets/button/basic_app_button.dart';
 import 'package:spotify/core/configs/assets/app_images.dart';
 import 'package:spotify/core/configs/assets/app_vectors.dart';
 import 'package:spotify/core/configs/theme/app_colors.dart';
+import 'package:spotify/features/choose_mode/presentation/views/choose_mode.dart';
 
 class GetStartedView extends StatelessWidget {
   const GetStartedView({super.key});
@@ -53,7 +54,14 @@ class GetStartedView extends StatelessWidget {
                 ),
                 BasicAppButton(
                   title: 'Get started',
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const ChooseModeView(),
+                      ),
+                    );
+                  },
                 ),
               ],
             ),
