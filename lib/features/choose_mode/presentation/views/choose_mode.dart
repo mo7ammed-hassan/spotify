4a256421567894f615d3cf72a15619be45ded242
mohamedhassan <mohamed7github@gmail.com>
2024-09-23 23:02:07 +0300

    add sun and moon vector to assets folder

--- lib/features/choose_mode/presentation/views/choose_mode.dart
+++ lib/features/choose_mode/presentation/views/choose_mode.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify/common/widgets/button/basic_app_button.dart';
 import 'package:spotify/core/configs/assets/app_images.dart';
+import 'package:spotify/core/configs/assets/app_vectors.dart';
+import 'package:spotify/core/configs/theme/app_colors.dart';
 
 class ChooseModeView extends StatelessWidget {
   const ChooseModeView({super.key});
@@ -16,7 +20,48 @@ class ChooseModeView extends StatelessWidget {
                 image: AssetImage(AppImages.chooseModeBG),
               ),
             ),
-          )
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.15),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 40),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: SvgPicture.asset(AppVectors.logo),
+                ),
+                const Spacer(),
+                const Text(
+                  'Choose mode',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+                const SizedBox(
+                  height: 21,
+                ),
+                //
+                const SizedBox(
+                  height: 50,
+                ),
+                BasicAppButton(
+                  title: 'Continue',
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const ChooseModeView(),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
