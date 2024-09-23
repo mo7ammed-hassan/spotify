52a224b3a1545219be95e8bb3291bc11c6bf1cf1
mohamedhassan <mohamed7github@gmail.com>
2024-09-23 22:33:28 +0300

    create choose mode feature

---
+++ lib/features/choose_mode/presentation/views/choose_mode.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChooseModeView extends StatelessWidget {
+  const ChooseModeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
