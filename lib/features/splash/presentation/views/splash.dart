37e548c8bd5ecacd0adbdcf745d40cd0915c66fb
mohamedhassan <mohamed7github@gmail.com>
2024-09-23 21:39:44 +0300

    make navigator from splash to get started view

--- lib/features/splash/presentation/views/splash.dart
+++ lib/features/splash/presentation/views/splash.dart
@@ -1,15 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:spotify/core/configs/assets/app_vectors.dart';
+import 'package:spotify/features/intro/presentation/views/get_started.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+    redirect();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: SvgPicture.asset(AppVectors.logo),
+      ),
+    );
+  }
+
+  Future<void> redirect() async {
+    await Future.delayed(const Duration(seconds: 2));
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const GetStartedView(),
       ),
     );
   }
