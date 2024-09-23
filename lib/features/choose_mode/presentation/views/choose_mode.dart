a6731534d265b8e3ec5d8ab5e11d96a8b138f157
mohamedhassan <mohamed7github@gmail.com>
2024-09-23 22:45:01 +0300

    display choose mode bg image

--- lib/features/choose_mode/presentation/views/choose_mode.dart
+++ lib/features/choose_mode/presentation/views/choose_mode.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:spotify/core/configs/assets/app_images.dart';
 
 class ChooseModeView extends StatelessWidget {
   const ChooseModeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Stack(
         children: [
-          
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage(AppImages.chooseModeBG),
+              ),
+            ),
+          )
         ],
       ),
     );
