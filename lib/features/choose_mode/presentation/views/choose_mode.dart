b89f2d6dcf838bcc9b214511af96201b49df3542
mohamedhassan <mohamed7github@gmail.com>
2024-09-24 20:05:00 +0300

    use navigator helper in choose mode view

--- lib/features/choose_mode/presentation/views/choose_mode.dart
+++ lib/features/choose_mode/presentation/views/choose_mode.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify/common/helper/navigator.dart';
 import 'package:spotify/common/widgets/button/basic_app_button.dart';
 import 'package:spotify/core/configs/assets/app_images.dart';
 import 'package:spotify/core/configs/assets/app_vectors.dart';
@@ -52,11 +53,8 @@ class ChooseModeView extends StatelessWidget {
                 BasicAppButton(
                   title: 'Continue',
                   onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const SignupOrSigninView(),
-                      ),
+                    context.push(
+                      const SignupOrSigninView(),
                     );
                   },
                 ),
