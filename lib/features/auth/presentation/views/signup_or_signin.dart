d63cecf96e7502a95b98aa018dcd9bd131a42cdb
mohamedhassan <mohamed7github@gmail.com>
2024-09-24 06:56:32 +0300

    display top and bottom pattern vector

--- lib/features/auth/presentation/views/signup_or_signin.dart
+++ lib/features/auth/presentation/views/signup_or_signin.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify/core/configs/assets/app_vectors.dart';
 
 class SignupOrSigninView extends StatelessWidget {
   const SignupOrSigninView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.topRight,
+            child: SvgPicture.asset(AppVectors.topPattern),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: SvgPicture.asset(AppVectors.bottomPattern),
+          ),
+        ],
+      ),
+    );
   }
 }
