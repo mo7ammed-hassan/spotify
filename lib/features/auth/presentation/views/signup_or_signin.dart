58ad159d61417175481944d8c85f2423d09056b8
mohamedhassan <mohamed7github@gmail.com>
2024-09-24 07:21:57 +0300

    finish signin or signup view

--- lib/features/auth/presentation/views/signup_or_signin.dart
+++ lib/features/auth/presentation/views/signup_or_signin.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify/common/widgets/button/basic_app_button.dart';
 import 'package:spotify/core/configs/assets/app_images.dart';
 import 'package:spotify/core/configs/assets/app_vectors.dart';
+import 'package:spotify/core/configs/theme/app_colors.dart';
 
 class SignupOrSigninView extends StatelessWidget {
   const SignupOrSigninView({super.key});
@@ -22,6 +24,71 @@ class SignupOrSigninView extends StatelessWidget {
           Align(
             alignment: Alignment.bottomLeft,
             child: Image.asset(AppImages.authBG),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(AppVectors.logo),
+                  const SizedBox(
+                    height: 55,
+                  ),
+                  const Text(
+                    'Enjoy Listening To Music',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 26,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 21,
+                  ),
+                  const Text(
+                    'Spotify is a proprietary Swedish audio streaming and media services provider',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      color: AppColors.grey,
+                      fontSize: 17,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 35,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: BasicAppButton(
+                          title: 'Register',
+                          onPressed: () {},
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 30,
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'Sign in',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
           ),
         ],
       ),
