67119980ee1f75e5d76cf869df7d9b3afb6929b8
mohamedhassan <mohamed7github@gmail.com>
2024-09-26 10:36:45 +0300

    create service locator

---
+++ lib/features/auth/data/repository/auth/auth_repository_impl.dart
@@ -0,0 +1,15 @@
+import 'package:spotify/features/auth/domain/repository/auth/auth.dart';
+
+class AuthRepositoryImpl extends AuthRepository {
+  @override
+  Future<void> signin() {
+    // TODO: implement signin
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signup() {
+    // TODO: implement signup
+    throw UnimplementedError();
+  }
+}
