d25caed19619e4372e9c605900d2af06a3ec0e73
mohamedhassan <mohamed7github@gmail.com>
2024-09-26 15:55:51 +0300

    implemet signin method in AuthRepositoryImpl

--- lib/features/auth/data/repository/auth/auth_repository_impl.dart
+++ lib/features/auth/data/repository/auth/auth_repository_impl.dart
@@ -1,14 +1,14 @@
 import 'package:dartz/dartz.dart';
 import 'package:spotify/features/auth/data/models/auth/create_user_req.dart';
+import 'package:spotify/features/auth/data/models/auth/signin_user_req.dart';
 import 'package:spotify/features/auth/data/sources/auth/auth_firebase_service.dart';
 import 'package:spotify/features/auth/domain/repository/auth/auth.dart';
 import 'package:spotify/service_locator.dart';
 
 class AuthRepositoryImpl extends AuthRepository {
   @override
-  Future<Either> signin() {
-    // TODO: implement signin
-    throw UnimplementedError();
+  Future<Either> signin(SigninUserReq signinUserReq) async {
+    return await getIt<AuthFirebaseService>().signin(signinUserReq);
   }
 
   @override
--- lib/features/auth/domain/repository/auth/auth.dart
+++ lib/features/auth/domain/repository/auth/auth.dart
@@ -1,8 +1,9 @@
 import 'package:dartz/dartz.dart';
 import 'package:spotify/features/auth/data/models/auth/create_user_req.dart';
+import 'package:spotify/features/auth/data/models/auth/signin_user_req.dart';
 
 abstract class AuthRepository {
   Future<Either> signup(CreateUserReq createUserReq);
 
-  Future<Either> signin();
+  Future<Either> signin(SigninUserReq signinUserReq);
 }
