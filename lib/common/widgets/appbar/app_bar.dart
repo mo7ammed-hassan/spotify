d095ddc40c3597557f077dde61748652b0264b4e
mohamedhassan <mohamed7github@gmail.com>
2024-09-27 15:36:23 +0300

    refactor appbar .. add appbar for home view

--- lib/common/widgets/appbar/app_bar.dart
+++ lib/common/widgets/appbar/app_bar.dart
@@ -4,9 +4,11 @@ import 'package:spotify/common/helper/navigator.dart';
 
 class BasicAppBar extends StatelessWidget implements PreferredSizeWidget {
   final Widget? title;
+  final bool hideBack;
   const BasicAppBar({
     super.key,
     this.title,
+    this.hideBack = false,
   });
 
   @override
@@ -16,28 +18,30 @@ class BasicAppBar extends StatelessWidget implements PreferredSizeWidget {
       elevation: 0,
       centerTitle: true,
       title: title ?? const Text(''),
-      leading: IconButton(
-        onPressed: () {
-          context.pop();
-        },
-        icon: Container(
-          height: 50,
-          width: 50,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: context.isDarkMode
-                ? Colors.white.withOpacity(0.03)
-                : Colors.black.withOpacity(0.04),
-          ),
-          child: Icon(
-            Icons.arrow_back_ios_new,
-            size: 17,
-            color: context.isDarkMode
-                ? const Color(0xffDDDDDD)
-                : const Color(0xff414141),
-          ),
-        ),
-      ),
+      leading: hideBack
+          ? null
+          : IconButton(
+              onPressed: () {
+                context.pop();
+              },
+              icon: Container(
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: context.isDarkMode
+                      ? Colors.white.withOpacity(0.03)
+                      : Colors.black.withOpacity(0.04),
+                ),
+                child: Icon(
+                  Icons.arrow_back_ios_new,
+                  size: 17,
+                  color: context.isDarkMode
+                      ? const Color(0xffDDDDDD)
+                      : const Color(0xff414141),
+                ),
+              ),
+            ),
     );
   }
 
--- lib/features/home/presentation/views/home.dart
+++ lib/features/home/presentation/views/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:spotify/common/widgets/appbar/app_bar.dart';
+import 'package:spotify/core/configs/assets/app_vectors.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -6,7 +9,14 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: BasicAppBar(
+        hideBack: true,
+        title: SvgPicture.asset(
+          AppVectors.logo,
+          width: 38,
+          height: 38,
+        ),
+      ),
     );
   }
 }
