bc1db695dddf7cd95b647651cb96e9f8a8d9675c
mohamedhassan <mohamed7github@gmail.com>
2024-09-27 21:15:58 +0300

    finish home top card

--- lib/features/home/presentation/views/home.dart
+++ lib/features/home/presentation/views/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:spotify/common/widgets/appbar/app_bar.dart';
+import 'package:spotify/core/configs/assets/app_images.dart';
 import 'package:spotify/core/configs/assets/app_vectors.dart';
 
 class HomeView extends StatelessWidget {
@@ -15,6 +16,44 @@ class HomeView extends StatelessWidget {
           AppVectors.logo,
           width: 38,
           height: 38,
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _homeTopCard(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _homeTopCard() {
+    return Center(
+      child: SizedBox(
+        height: 140,
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SvgPicture.asset(
+                AppVectors.homeTopCard,
+              ),
+            ),
+            Positioned(
+              top: -40,
+              right: 0,
+              bottom: 0,
+              child: Padding(
+                padding: const EdgeInsets.only(right: 45),
+                child: Image.asset(
+                  AppImages.homeArtist,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
