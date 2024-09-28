b7f7469209e7ab3e3339975086e7db1cb635549b
mohamedhassan <mohamed7github@gmail.com>
2024-09-28 10:27:08 +0300

    register SongsFirebaseService and usecae

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -5,7 +5,9 @@ import 'package:spotify/features/auth/domain/repository/auth.dart';
 import 'package:spotify/features/auth/domain/use_cases/signin.dart';
 import 'package:spotify/features/auth/domain/use_cases/signup.dart';
 import 'package:spotify/features/home/data/repository/song.dart';
+import 'package:spotify/features/home/data/sources/songs_firebase_service.dart';
 import 'package:spotify/features/home/domain/repository/song_repository.dart';
+import 'package:spotify/features/home/domain/use_cases/get_newst_songs.dart';
 
 final getIt = GetIt.instance;
 
@@ -13,6 +15,10 @@ Future<void> initializeDependencies() async {
   // Register services
   getIt.registerSingleton<AuthFirebaseService>(
     AuthFirebaseServiceImpl(),
+  );
+
+  getIt.registerSingleton<SongsFirebaseService>(
+    SongsFirebaseServiceImpl(),
   );
 
   getIt.registerSingleton<AuthRepository>(
@@ -27,8 +33,11 @@ Future<void> initializeDependencies() async {
     SigninUseCase(),
   );
 
-
   getIt.registerSingleton<SongRepository>(
     SongRepositoryImpl(),
+  );
+
+  getIt.registerSingleton<GetNewstSongsUseCase>(
+    GetNewstSongsUseCase(),
   );
 }
