be2496f9092362e8072a5dd377e0684130f635d7
mohamedhassan <mohamed7github@gmail.com>
2024-09-30 15:57:08 +0300

    finish song player cuit

--- lib/features/song_player/presentation/cubits/song_player_cubit.dart
+++ lib/features/song_player/presentation/cubits/song_player_cubit.dart
@@ -1,6 +1,61 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:spotify/features/song_player/presentation/cubits/song_player_state.dart';
 
 class SongPlayerCubit extends Cubit<SongPlayerState> {
-  SongPlayerCubit() : super(SongPlayerLoading());
+  AudioPlayer audioPlayer = AudioPlayer();
+
+  Duration audioDuration = Duration.zero;
+  Duration audioPosition = Duration.zero;
+
+  SongPlayerCubit() : super(SongPlayerLoading()) {
+    audioPlayer.positionStream.listen(
+      (position) {
+        audioPosition = position;
+        updateSongPlayer();
+      },
+    );
+    audioPlayer.durationStream.listen(
+      (duration) {
+        audioDuration = duration!;
+      },
+    );
+  }
+
+  void updateSongPlayer() {
+    emit(
+      SongPalyerLoaded(),
+    );
+  }
+
+  void loadSong(String songUrl) async {
+    try {
+      await audioPlayer.setUrl(songUrl);
+      emit(
+        SongPalyerLoaded(),
+      );
+    } on Exception {
+      emit(
+        SongPlayerFailure(),
+      );
+    }
+  }
+
+  void playSongOrPause(String songUrl) async {
+    if (audioPlayer.playing) {
+      audioPlayer.pause();
+    } else {
+      audioPlayer.play();
+    }
+    emit(
+      SongPalyerLoaded(),
+    );
+  }
+
+  // dispose audio palyer when bloc is close
+  @override
+  Future<void> close() {
+    audioPlayer.dispose();
+    return super.close();
+  }
 }
