da878560235a799b5b253d72a53bdf48a4ad8d68
mohamedhassan <mohamed7github@gmail.com>
2024-10-01 19:25:03 +0300

    add songs icons

--- lib/core/configs/assets/app_vectors.dart
+++ lib/core/configs/assets/app_vectors.dart
@@ -9,5 +9,10 @@ class AppVectors {
   static const String bottomPattern = '${basePath}bottom_pattern$format';
   static const String homeTopCard = '${basePath}home_top_card$format';
   static const String search = '${basePath}search$format';
-  static const String profileTopPattern = '${basePath}profile_top_pattern$format';
+  static const String profileTopPattern =
+      '${basePath}profile_top_pattern$format';
+  static const String playNext = '${basePath}play_next$format';
+  static const String playPrevious = '${basePath}play_previous$format';
+  static const String repeat = '${basePath}repeat$format';
+  static const String shuffle = '${basePath}shuffle$format';
 }
