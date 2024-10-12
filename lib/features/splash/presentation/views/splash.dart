8759ee236daa821b3310e685aff1bacb9045044c
mohamedhassan <mohamed7github@gmail.com>
2024-10-12 18:22:10 +0300

    add animation for splash view

--- lib/features/splash/presentation/views/splash.dart
+++ lib/features/splash/presentation/views/splash.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:spotify/core/configs/assets/app_vectors.dart';
-import 'package:spotify/features/intro/presentation/views/get_started.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({super.key});
@@ -10,10 +9,18 @@ class SplashView extends StatefulWidget {
   State<SplashView> createState() => _SplashViewState();
 }
 
-class _SplashViewState extends State<SplashView> {
+class _SplashViewState extends State<SplashView>
+    with SingleTickerProviderStateMixin {
+  AnimationController? animationController;
+  Animation? fadingAnimation;
   @override
   void initState() {
     super.initState();
+    animationController =
+        AnimationController(duration: Duration(seconds: 2), vsync: this);
+    fadingAnimation =
+        Tween<double>(begin: 0.1, end: 1.0).animate(animationController!);
+    animationController?.forward();
     redirect();
   }
 
@@ -21,18 +28,24 @@ class _SplashViewState extends State<SplashView> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: SvgPicture.asset(AppVectors.logo),
+        child: AnimatedBuilder(
+          animation: fadingAnimation!,
+          builder: (context, _) => Opacity(
+            opacity: fadingAnimation?.value,
+            child: SvgPicture.asset(AppVectors.logo),
+          ),
+        ),
       ),
     );
   }
 
   Future<void> redirect() async {
-    await Future.delayed(const Duration(seconds: 2));
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(
-        builder: (context) => const GetStartedView(),
-      ),
-    );
+    await Future.delayed(const Duration(seconds: 4));
+    // Navigator.pushReplacement(
+    //   context,
+    //   MaterialPageRoute(
+    //     builder: (context) => const GetStartedView(),
+    //   ),
+    // );
   }
 }
